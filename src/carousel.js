260ca45ec403a3d60f050455c3cd1164f43e0675
OlaniyanEniola <olaniyaneniola0225@gmail.com>
2022-12-30 08:48:02 +0100

    added carousel to hero text and refactored code

--- src/carousel.js
+++ src/carousel.js
@@ -1,33 +1,35 @@
-const track = document.querySelector('.carousel__track');
-const slides = Array.from(track.children);
-const nextBtns = document.querySelectorAll('.carousel__button--next');
-const prevBtns = document.querySelectorAll('.carousel__button--previous');
+const tracks = document.querySelectorAll('.carousel__track');
+tracks.forEach((track) => {
+	const slides = Array.from(track.children);
+	const nextBtns = document.querySelectorAll('.carousel__button--next');
+	const prevBtns = document.querySelectorAll('.carousel__button--previous');
 
-const slideWidth = slides[0].getBoundingClientRect().width;
+	const slideWidth = slides[0].getBoundingClientRect().width;
 
-const setSlidePosition = (slide, index) => {
-    slide.style.left = slideWidth * index + 'px';
-}
-slides.forEach(setSlidePosition);
+	const setSlidePosition = (slide, index) => {
+		slide.style.left = slideWidth * index + 'px';
+	}
+	slides.forEach(setSlidePosition);
 
-const moveToSlide = (track, currentSlide, targetSlide) => {
-    track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
-    currentSlide.classList.remove('current-slide');
-    targetSlide.classList.add('current-slide');
-}
+	const moveToSlide = (track, currentSlide, targetSlide) => {
+		track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+		currentSlide.classList.remove('current-slide');
+		targetSlide.classList.add('current-slide');
+	}
 
-prevBtns.forEach(prevBtn => {
-	prevBtn.addEventListener('click', e => {
-	    const currentSlide = track.querySelector('.current-slide');
-	    const prevSlide = currentSlide.previousElementSibling;  
-	    moveToSlide(track, currentSlide, prevSlide);
+	prevBtns.forEach(prevBtn => {
+		prevBtn.addEventListener('click', e => {
+			const currentSlide = track.querySelector('.current-slide');
+			const prevSlide = currentSlide.previousElementSibling;  
+			moveToSlide(track, currentSlide, prevSlide);
+		});
 	});
-});
 
-nextBtns.forEach(nextBtn => {
-	nextBtn.addEventListener('click', e => {
-	    const currentSlide = track.querySelector('.current-slide');
-	    const nextSlide = currentSlide.nextElementSibling;  
-	    moveToSlide(track, currentSlide, nextSlide);
+	nextBtns.forEach(nextBtn => {
+		nextBtn.addEventListener('click', e => {
+			const currentSlide = track.querySelector('.current-slide');
+			const nextSlide = currentSlide.nextElementSibling;  
+			moveToSlide(track, currentSlide, nextSlide);
+		});
 	});
 });
