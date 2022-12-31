eb2e691c5b19af88e078be4fc850afa885cdbaac
OlaniyanEniola <olaniyaneniola0225@gmail.com>
2022-12-31 20:38:00 +0100

    added animation for mobile nav

--- src/mobile-nav.js
+++ src/mobile-nav.js
@@ -1,11 +1,13 @@
 const navContainer = document.querySelector('.mobile__nav-container');
+const nav = navContainer.querySelector('.mobile__nav');
 const burger = document.querySelector('#burger');
 const closeNav = navContainer.querySelector('#close');
 
 burger.addEventListener('click', (e) => {
     navContainer.classList.remove('is-hidden');
+    nav.classList.remove('slide-in');
 });
 closeNav.addEventListener('click', (e) => {
     navContainer.classList.add('is-hidden');
+    nav.classList.add('slide-in');
 });
-
